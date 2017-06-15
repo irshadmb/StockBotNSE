5ee8ce362533985584824f97f5dafd67d01a64ca
irshadmb <irshadmb@gmail.com>
2017-06-15 19:52:48 +0530

    Open Shift Support

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var request = require('request');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+server.listen(port,ipaddress, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
 
