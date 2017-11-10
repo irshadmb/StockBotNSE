0cb81a2d0d127df8025b27a856d7df16220465f2
irshadmb <irshadmb@gmail.com>
2017-11-10 11:16:36 +0530

    Added Microsoft APP ID

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ server.listen(port,ip, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-
+	appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 
